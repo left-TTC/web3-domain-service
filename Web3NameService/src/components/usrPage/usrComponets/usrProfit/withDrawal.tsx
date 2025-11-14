6c08d8500f54598e554672b98649ee56732633fd
left-TTC <leftear1321@gmail.com>
2025-11-14 20:55:31 +0800

    internet test

--- Web3NameService/src/components/usrPage/usrComponets/usrProfit/withDrawal.tsx
+++ Web3NameService/src/components/usrPage/usrComponets/usrProfit/withDrawal.tsx
@@ -3,6 +3,11 @@ import { useWalletEnv } from "@/provider/walletEnviroment/useWalletEnv"
 import { useConnection } from "@solana/wallet-adapter-react"
 import { extractUserProfit } from "../usrAuction/function/extractUserProfit"
 
+import "@/style/components/usrPage/usrComponents/usrProfit/withDrawal.css"
+
+import profit from "@/assets/profit.svg"
+import volume from "@/assets/volume.svg"
+
 interface WithDrawalProps {
     usrProfit: number | null,
     usrVolume: number | null,
@@ -20,15 +25,37 @@ const WithDrawal: React.FC<WithDrawalProps> = ({
         <div className="profitwithdrawal">
             
             <div className="profitandvolume">
-                <h1>profit: {usrProfit? `${(usrProfit / 1e9).toFixed(4)} SOL`:"0"}</h1>
-                <h2>volume: {usrVolume? `${(usrVolume / 1e9).toFixed(4)} SOL`:"0"}</h2>
+                <div className="profitShow">
+                    <div className="profitimgcon">
+                        <img src={profit} className="profiticon" />
+                    </div>
+                    <div className="profitwordcon">
+                        <h1>profit: </h1>
+                        <h2>{usrProfit? `${(usrProfit / 1e9).toFixed(4)} SOL`:"0"}</h2>
+                    </div>
+                    <button 
+                        className="extractbu"
+                        onClick={() => extractUserProfit(signTransaction,
+                            user, solanaToast, connection
+                        )}
+                    >
+                        <h1>extract</h1>
+                    </button>
+                </div>
+                <div className="profitwithdrawalline" />
+                <div className="profitShow">
+                    <div className="profitimgcon">
+                        <img src={volume} className="volumeicon" />
+                    </div>
+                    <div className="profitwordcon">
+                        <h1>volume: </h1>
+                        <h2>{usrVolume? `${(usrVolume / 1e9).toFixed(4)} SOL`:"0"}</h2>
+                    </div>
+                </div>
+                <div className="profitwithdrawalline" />
             </div>
 
-            <button onClick={() => extractUserProfit(signTransaction,
-                user, solanaToast, connection
-            )}>
-                extract 0.01 SOL
-            </button>
+            
         </div>
     )
 }
